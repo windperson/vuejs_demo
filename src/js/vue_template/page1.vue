55c453f9274bce28a10d7bc0db2e930bf2e5db7b
windperson <windperson@gmail.com>
2017-01-13 02:04:32 +0800

    add toggle animation demo

--- src/js/vue_template/page1.vue
+++ src/js/vue_template/page1.vue
@@ -6,6 +6,19 @@
             </p>
             <button v-on:click="counter_up">+ 1</button>
             <br/>
+            <hr/>
+            <div v-on:click="changeShowHeader"><span>toggle</span>
+                <transition name="fade">
+                    <span v-if="show_status_toggle">&nbsp;▲&nbsp;</span>
+                    <span v-else>&nbsp;▼&nbsp;</span>
+                </transition>
+            </div>
+            <transition name="fade">
+                <div v-if="show_status_toggle">
+                    <span>Header Shown</span>
+                </div>
+            </transition>
+            <hr/>
             <button v-on:click="next_page">next page</button>
         </div>
     </transition>
@@ -14,20 +27,37 @@
 <script>
     export default {
         name: "page1",
-        props: ['data','eventbus'],
+        props: ['data', 'eventbus'],
         data: function () {
             return {
-                my_counter: 0
+                my_counter: 0,
+                show_status: false
             };
         },
         methods: {
             next_page: function () {
-                this.eventbus.$emit("test",{src: "page1", dst:"page2"});                
-                this.$router.push({name: "page-2"});
+                this.eventbus.$emit("test", { src: "page1", dst: "page2" });
+                this.$router.push({ name: "page-2" });
             },
             counter_up: function () {
                 this.my_counter++;
+            },
+            changeShowHeader: function () {
+                this.show_status = !this.show_status;
+            }
+        },
+        computed: {
+            show_status_toggle: function () {
+                return this.show_status;
             }
         }
     }
 </script>
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .2s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
+}
+</style>
